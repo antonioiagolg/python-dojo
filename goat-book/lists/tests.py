481f9bd0f43aa402248dd29e2a2cae00406b9a2e
Antonio Iago <antonio.iagolg@gmail.com>
2025-05-28 17:32:24 +0200

    [goat-book]: Url config, tests

--- goat-book/lists/tests.py
+++ goat-book/lists/tests.py
@@ -1,5 +1,13 @@
 from django.test import TestCase
+from django.http import HttpRequest
 
-class SmokeTest(TestCase):
-    def test_fail(self):
-        self.assertEqual(1 + 1, 3)
+from lists.views import home_page
+
+class HomePageTest(TestCase):
+    def test_home_page_returns_correct_html(self):
+        res = self.client.get('/')
+        self.assertContains(res, '<html>')
+        self.assertContains(res, '<title>To-Do lists</title>')
+        self.assertContains(res, '</html>')
+
+
